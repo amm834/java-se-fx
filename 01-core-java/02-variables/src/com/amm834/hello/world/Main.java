4531dc13e1c3f44228a1b4b7e9e46db302c33431
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-05-12 16:53:31 +0630

    variabe

---
+++ 01-core-java/02-variables/src/com/amm834/hello/world/Main.java
@@ -0,0 +1,12 @@
+package com.amm834.hello.world;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String name; // declare
+        name = "Mg Mg"; // assign
+
+        System.out.println("Hello, " + name);
+        System.out.println(name + " likes cake");
+    }
+}
