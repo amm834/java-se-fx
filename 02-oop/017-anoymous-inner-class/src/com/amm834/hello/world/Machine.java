7004a164b94299acdb377ae12e9bd8b9ee888f88
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-05-13 01:01:42 +0630

    Anonymous inner class

---
+++ 02-oop/017-anoymous-inner-class/src/com/amm834/hello/world/Machine.java
@@ -0,0 +1,11 @@
+package com.amm834.hello.world;
+
+public class Machine {
+    public void start() {
+        System.out.println("Car is starting");
+    }
+
+    public void stop() {
+        System.out.println("Car is stopping");
+    }
+}
