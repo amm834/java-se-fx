25576f3f935c5a3bc37b72e7e79a2a15e560f3cb
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-05-12 20:11:08 +0630

    Parsing string to number using Integer.parseInt

--- 01-core-java/025-scanner-class/src/com/amm834/hello/world/Main.java
+++ 01-core-java/025-scanner-class/src/com/amm834/hello/world/Main.java
@@ -7,9 +7,18 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter your age: ");
+        var ageString = scanner.nextLine();
         System.out.print("Enter your name: ");
         String name = scanner.nextLine();
 
         System.out.println("Your name " + name);
+        int age = Integer.parseInt(ageString);
+        if (age > 18) {
+            System.out.print("You are adult");
+        } else {
+            System.out.print("You are child");
+        }
+
     }
 }
