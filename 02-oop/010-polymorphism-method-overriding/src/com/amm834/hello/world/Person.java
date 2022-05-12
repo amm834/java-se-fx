7b74fbd2cd05c43e1d290e54c751fdb395e74a97
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-05-13 00:16:39 +0630

    Method overloading

---
+++ 02-oop/010-polymorphism-method-overriding/src/com/amm834/hello/world/Person.java
@@ -0,0 +1,14 @@
+package com.amm834.hello.world;
+
+public class Person {
+
+    public void showInfo(String name) {
+        System.out.println("I am " + name);
+    }
+
+    public void showInfo(int id, String name) {
+        System.out.println("ID " + id);
+        System.out.println("I am " + name);
+    }
+
+}
