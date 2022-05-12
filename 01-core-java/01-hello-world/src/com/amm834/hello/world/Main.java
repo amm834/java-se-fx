8d84a6bf671aab5142dfaa30b70bdd7cc9253c32
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-05-12 16:49:09 +0630

    Comment

--- 01-core-java/01-hello-world/src/com/amm834/hello/world/Main.java
+++ 01-core-java/01-hello-world/src/com/amm834/hello/world/Main.java
@@ -3,6 +3,12 @@ package com.amm834.hello.world;
 public class Main {
 
     public static void main(String[] args) {
+        // This is a comment
+
+        /*
+        This is multi
+                       line comment
+         */
         System.out.println("New Line \n A Tab \t A Back Slash \\");
     }
 }
