9fc326106fe2cd49b46b34c0d4c8d0663b314cc8
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-05-12 20:55:28 +0630

    constructor

---
+++ 02-oop/04-constructor/src/com/amm834/hello/world/Main.java
@@ -0,0 +1,10 @@
+package com.amm834.hello.world;
+
+public class Main {
+
+    public static void main(String[] args) {
+        var person = new Person("Mg Mg");
+        var name = person.getName();
+        System.out.println(name);
+    }
+}
---
+++ 02-oop/04-constructor/src/com/amm834/hello/world/Person.java
@@ -0,0 +1,17 @@
+package com.amm834.hello.world;
+
+public class Person {
+    private String name;
+
+    Person(String name) {
+        this.name = name;
+    }
+
+    public Person() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+}
