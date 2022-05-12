ce8f9a3aa1994f41e7a4aa96dbb46b4cc74ea023
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-05-12 20:43:17 +0630

    class and instance object

---
+++ 02-oop/01-class-and-object/src/com/amm834/hello/world/Student.java
@@ -0,0 +1,12 @@
+package com.amm834.hello.world;
+
+public class Student {
+    public String name;
+    public int age;
+
+    public void introduce() {
+        System.out.println("My name is " + name);
+        System.out.println("My age is " + age);
+    }
+
+}
