418b5c4c350f784102450cb2ea08d4454435fcb3
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-05-12 16:48:23 +0630

    Escape character

--- 01-core-java/01-hello-world/src/com/amm834/hello/world/Main.java
+++ 01-core-java/01-hello-world/src/com/amm834/hello/world/Main.java
@@ -3,7 +3,6 @@ package com.amm834.hello.world;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.print("This is non \\n terminated method");
+        System.out.println("New Line \n A Tab \t A Back Slash \\");
     }
 }
