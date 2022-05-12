f7a7ec47fc6cb126fa8ef4fb69e7f38619d9af91
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-05-12 16:59:07 +0630

    Primitive data types

---
+++ 01-core-java/03-primitive-types/src/com/amm834/hello/world/Main.java
@@ -0,0 +1,13 @@
+package com.amm834.hello.world;
+
+public class Main {
+
+    public static void main(String[] args) {
+        byte byteInteger = 123;
+        int integer = 123456;
+        long longInterger = 9999999999999999L;
+        System.out.println("Long Interger" + longInterger);
+        System.out.println("Default Interger" + integer);
+        System.out.println("Byte Interger" + byteInteger);
+    }
+}
