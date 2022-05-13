baadd696d3302167fe3612590a150f142f72dde8
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-05-14 00:11:14 +0630

    custom type comparator

--- 06-collectin-framework/04-comparator/src/com/amm834/hello/world/Main.java
+++ 06-collectin-framework/04-comparator/src/com/amm834/hello/world/Main.java
@@ -20,5 +20,16 @@ public class Main {
         });
 
         numbers.forEach(System.out::println);
+
+
+        System.out.println("/////// String Comparator /////////");
+        var names = new ArrayList<String>();
+        names.add("Mg Mg");
+        names.add("Aung Aung");
+        names.add("Hla Hla");
+        names.add("Ma Ma");
+
+        names.sort((name1, name2) -> -name1.compareTo(name2));
+        names.forEach(System.out::println);
     }
 }
---
+++ 06-collectin-framework/05-compare-with-custom-type/src/com/amm834/hello/world/Person.java
@@ -0,0 +1,11 @@
+package com.amm834.hello.world;
+
+public class Person {
+    public final int id;
+    public final String name;
+
+    Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
